9cfb27542ee6ee0bd96095b829290eacfa4bf16b
= <=>
2024-07-07 22:12:38 +0530

    Initial Commit

---
+++ src/package1/A.java
@@ -0,0 +1,21 @@
+package package1;
+
+public class A {
+    
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(x);
+        x = m(x);
+        System.out.println(x);
+    }
+    
+    public static int m(int x){
+        
+        return x+5;
+    }
+    
+}
+
+//---------------------------------------------------------------------
+
+
